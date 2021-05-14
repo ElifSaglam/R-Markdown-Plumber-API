4842c2dc73a30e6838e4e3f31aed5d459c222674
Elif Sağlam <esaglam@bib-web.com>
2021-05-14 22:16:23 +0300

    Markdwon api eklendi.

---
+++ plumber.R
@@ -0,0 +1,17 @@
+
+#### libraries ####
+require(rmarkdown)
+require(plumber)
+#### End Region ####
+
+#------------------------------------ API------------------------------------------
+
+
+#* @apiTitle Plumber Example API
+#* @serializer contentType list(type="application/html")
+#* @get /markdown
+function(res){
+  
+  include_rmd("R_Markdown.Rmd", res)
+}
+
