4e8902cf1d7bf4acbe1d37ba7b70f388198900f3
ElifSaglam <61660262+ElifSaglam@users.noreply.github.com>
2021-09-20 17:12:10 +0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -15,3 +15,10 @@ function(res){
   include_rmd("R_Markdown.Rmd", res)
 }
 
+#* @apiTitle Plumber Example API
+#* @serializer contentType list(type="application/html")
+#* @get /markdownPlumber
+function(res){
+  
+  include_rmd("MarkdownPlumber.Rmd", res)
+}
